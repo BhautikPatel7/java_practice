19402030faf48e132a18288b08d12228f09ae591
BhautikPatel7 <thummarbhautik7045@gmail.com>
2023-09-23 18:20:47 +0530

    Completed Portion Of String

--- Srting/stringcompression.java
+++ Srting/stringcompression.java
@@ -2,6 +2,22 @@ package Srting;
 
 public class stringcompression {
     public static void main(String[] args) {
-        
+      Integer count = 0;
+        String Str2 = "";
+         String str =  "aaaabbcccdd";
+         for (int i = 0; i < str.length(); i++) {
+           count = 1;
+           while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+            count ++;
+            i++;
+           }
+           Str2 +=  str.charAt(i);
+           if (count > 1) {
+            Str2 += count.toString();
+           }
+       }
+       for (int i = 0; i < Str2.length(); i++) {
+        System.out.print(Str2.charAt(i) + " ");
+       }
     }
 }
