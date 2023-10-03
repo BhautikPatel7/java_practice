f224af3ae1159c73a7a9303434b07d110afe4be2
BhautikPatel7 <thummarbhautik7045@gmail.com>
2023-10-04 00:18:05 +0530

    Starting Learning about ArrayList

---
+++ arraylist/first.java
@@ -0,0 +1,20 @@
+package arraylist;
+import java.util.ArrayList;
+
+public class first {
+    public static void main(String[] args) {
+        //first obj = new first();
+        // Java Collection Framework
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        
+        
+    }
+}
