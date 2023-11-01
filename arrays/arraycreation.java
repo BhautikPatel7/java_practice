7d23733a17c9728c7d8d3e6e08ab16a46434308e
BhautikPatel7 <thummarbhautik7045@gmail.com>
2023-11-01 22:40:54 +0530

    java syntax for arraycreation

--- arrays/arraycreation.java
+++ arrays/arraycreation.java
@@ -1,5 +1,5 @@
 public class arraycreation {
-    
+    //This Code for creating array in java may synatx is varry But processer is Same
     public static void main(String[] args) {
     int numn[] = { 1 ,2,3,4,5,6,7};
     update(numn);
